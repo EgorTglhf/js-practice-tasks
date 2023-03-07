b8ffec585b0a5b2c2665fd734767cb3fbf017d75
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2023-03-07 20:06:33 +0300

    implemented function which to getting inner string from element and return array

--- count-days-with-higher-temperature/script.js
+++ count-days-with-higher-temperature/script.js
@@ -1,30 +1,22 @@
 import count_days_stack from './count_days_stack.js';
 import count_days_two_loops from './count_days_two_loops.js';
 
-let daysTemperatureArray = l_i_1.innerText
-  .slice(1, l_i_1.innerText.length - 1)
-  .split(',')
-  .map((d) => parseInt(d));
-let result = count_days_two_loops(daysTemperatureArray);
+let result = count_days_two_loops(getArrayInnerText(l_i_1));
 l_o_1.innerText = '{' + result.join(', ') + '}';
 
-daysTemperatureArray = s_i_1.innerText
-  .slice(1, s_i_1.innerText.length - 1)
-  .split(',')
-  .map((d) => parseInt(d));
-result = count_days_stack(daysTemperatureArray);
+result = count_days_stack(getArrayInnerText(s_i_1));
 s_o_1.innerText = '{' + result.join(', ') + '}';
 
-daysTemperatureArray = l_i_2.innerText
-  .slice(1, l_o_2.innerText.length - 1)
-  .split(',')
-  .map((d) => parseInt(d));
-result = count_days_two_loops(daysTemperatureArray);
+result = count_days_two_loops(getArrayInnerText(l_i_2));
 l_o_2.innerText = '{' + result.join(', ') + '}';
 
-daysTemperatureArray = s_i_2.innerText
-  .slice(1, s_o_2.innerText.length - 1)
-  .split(',')
-  .map((d) => parseInt(d));
-result = count_days_stack(daysTemperatureArray);
+result = count_days_stack(getArrayInnerText(s_i_2));
 s_o_2.innerText = '{' + result.join(', ') + '}';
+
+function getArrayInnerText(el) {
+  const text = el.innerText;
+  return text
+    .slice(1, text.length - 1)
+    .split(',')
+    .map((d) => parseInt(d));
+}
